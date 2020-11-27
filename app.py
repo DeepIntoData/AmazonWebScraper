ce5fe13be8b64d8d05530bd2d1b3dc852bb95c29
Frozte <deeppatel@live.com>
2020-11-26 23:54:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ def foo():
     search_terms = search_term.split(" ")
 
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
 
     # chrome_options = webdriver.ChromeOptions()
     # chrome_options.binary_location = GOOGLE_CHROME_BIN
