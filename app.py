98e8ab3fb22e4cef22e5dd92ccc7657241943ab2
Frozte <deeppatel@live.com>
2020-11-30 18:24:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def foo():
     bar = request.form['test']
     
     #URL = "http://www.amazon.com/"
-    NUMBER_OF_PAGES_TO_SEARCH = 3
+    NUMBER_OF_PAGES_TO_SEARCH = 2
     QUESTION_PRODUCT = "What are you looking for?\n:"
     PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
     #ELEMENT_ID = '//*[@id="twotabsearchtextbox"]'
