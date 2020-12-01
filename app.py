5dd77b2e81812c6bb051f93dbe9af554dbcc167c
Frozte <deeppatel@live.com>
2020-11-30 22:41:11 -0500

    Cleaned File Structure/Added CSS Elements/Consolidated Code

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
+from price_scraper import scrape_amazon_search, best_deal
 from flask import Flask, request, render_template, jsonify
+import requests 
 import json
-import os
-import time
-#import random
-#from seleniumwire import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-from product import Product
-from utils import convert_price_toNumber
-from price_scraper import get_url
+import os 
 
 app = Flask(__name__)
 
@@ -19,189 +10,20 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
-@app.route('/get-text', methods=['GET', 'POST'])
-def foo():
+@app.route('/scrape', methods=['GET', 'POST'])
+def scrape():
 
-    print("")
-    print("#####################################")
-    start_time = time.time()
+    search_term = str(request.form['test'])
 
-    bar = request.form['test']
-    
-    #URL = "http://www.amazon.com/"
-    NUMBER_OF_PAGES_TO_SEARCH = 2
-    QUESTION_PRODUCT = "What are you looking for?\n:"
-    PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
-    #ELEMENT_ID = '//*[@id="twotabsearchtextbox"]'
-    #ELEMENT_ID = "//input[@type='text'][@id='twotabsearchtextbox']"
-    search_term = str(bar)
-    URL = get_url(search_term)
+    products = scrape_amazon_search(search_term)
 
-    biggest_discount = 0.0
-    lowest_price = 0.0
-    chepest_product = Product("", "", "", "", "", "")
-    best_deal_product = Product("", "", "", "", "", "")
-    #search_terms = search_term.split(" ")
-    #search_terms = search_term.replace(" ","+")
+    best_deal_item = best_deal(products)
 
-    print("")
-    print("--- DRIVER STARTED ---")
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome' #PATH SET ON HEROKU CONFIG_VARS
-    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver' #PATH SET ON HEROKU CONFIG_VARS
-
-    #####################################
-    options = webdriver.ChromeOptions()
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") #UNCOMMENT FOR DEPLOYMENT/COMMENT FOR TESTING
-    #####################################
-    options.add_argument("--no-sandbox")
-    options.add_argument('--headless')
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--incognito")
-    #####################################
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options) #UNCOMMENT FOR DEPLOYMENT
-    #driver = webdriver.Chrome("D:\chromedriver.exe", options=options) ##UNCOMMENT FOR TESTING (SET DRIVERT PATH)
-    #####################################
-
-    # headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WIN64; x64) AppleWebKit/537.36 Chrome/87.0.4280.66 Safari.537,36'}
-    # proxies_list = ["128.199.109.241:8080","113.53.230.195:3128","125.141.200.53:80","125.141.200.14:80","128.199.200.112:138","149.56.123.99:3128","128.199.200.112:80","125.141.200.39:80","134.213.29.202:4444"]
-    # proxies = {'https': random.choice(proxies_list)}
-    # time.sleep(0.5 * random.random())
-    
-    # driver.header_overrides = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WIN64; x64) AppleWebKit/537.36 Chrome/87.0.4280.66 Safari.537,36'}
-
-    driver.get(URL)
-    time.sleep(3)
-
-    print("")
-    print("--- SCRAPING... ---")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print("")
-    
-    # element = driver.find_element_by_xpath(ELEMENT_ID)
-    # element.send_keys(search_term)
-    # element.send_keys(Keys.ENTER)
-
-    products = []
-
-    page = NUMBER_OF_PAGES_TO_SEARCH
-
-    print("--- PAGES SCRAPED ---")
-    print(page)
-    while True:
-        if page != 0:
-            try:
-                driver.get(driver.current_url + "&page=" + str(page))
-            except:
-                break
-        
-        for i in driver.find_elements_by_xpath(PRODUCT_PATH):
-            should_add = True
-            name = ""
-            price = ""
-            prev_price = ""
-            link = ""
-            discount = 0.0
-            # rating = ""
-            prime = False
-            try:
-                h2tag = i.find_element_by_tag_name('h2')
-                name = h2tag.text
-                price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
-                link = h2tag.find_element_by_tag_name('a').get_attribute("href")
-                # rating = 
-                try:
-                    prime_element = i.find_element_by_class_name("a-icon-prime")
-                    #print(prime_element)
-                    prime = True
-                except:
-                    Exception()
-                try:
-                    prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
-                    discount = (prev_price-price)/prev_price*100
-                except:
-                    Exception()
-                    prev_price = price
-            except:
-                # print("exception")
-                should_add = False
-            
-            product = Product(name, price, prev_price, discount, link, prime)
-            if should_add:
-                products.append(product)
-                # print(products)
-                
-        page = page - 1
-        if page == 0:
-            break
-        print(page)
-    
-    driver.quit()
-    
-    print("")
-    print("--- DRIVER SUCCESS... STARTING SORTER ---")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    
-    run = 0
-    for product in products:
-        not_right = False
-        # for word in search_terms:
-        #     if word.lower() not in product.name.lower():
-        #         not_right = True
-        if not not_right:
-            if run == 0:
-                lowest_price = product.price
-                chepest_product = product
-                run = 1
-            elif product.price < lowest_price:
-                lowest_price = product.price
-                chepest_product = product
-            if product.discount > biggest_discount:
-                biggest_discount = product.discount
-                #print(product.discount)
-                best_deal_product = product
-
-    data = {}
-    data["Products"] = []
-    for prod in products:
-        data["Products"].append(prod.serialize())
-    
-    # with open('products.json', 'w') as json_file:
-    #     data = {}
-    #     data["Products"] = []
-    #     for prod in products:
-    #         data["Products"].append(prod.serialize())
-    #     json.dump(data, json_file, sort_keys=True, indent=4)
-
-    # print(json.dumps(chepest_product.serialize(), indent=4, sort_keys=True))
-    # print(json.dumps(best_deal_product.serialize(), indent=4, sort_keys=True))
-
-    # options = webdriver.ChromeOptions()
-    # options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # ##############################
-    # options.add_argument('--headless')
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-dev-shm-usage")
-    # #options.add_argument("--disable-gpu")
-    # ##############################
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
-    # #driver = webdriver.Chrome("chromedriver.exe", options=options)
-    # ##############################
-
-    # driver.get(best_deal_product.link)
-    # driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
-
-    print("")
-    print("--- SESSION COMPLETE ---")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print("")
-    print("#####################################")
-    
-    return jsonify(data)
+    #return jsonify(products)
+    return (best_deal_item)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- price_scraper.py
+++ price_scraper.py
@@ -1,128 +1,144 @@
+import requests 
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from utils import convert_price_toNumber
+import os
+
+##############################################################################
+
 def get_url(search_term):
     template = 'https://www.amazon.com/s?k={}'
     search_term = search_term.replace(' ', '+')
     return template.format(search_term)
 
-# import requests 
-# import json
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from product import Product
-# from utils import convert_price_toNumber
+##############################################################################
 
-#     bar = request.form['test']
+def scrape_amazon_search(search_term):
     
-#     URL = "http://www.amazon.com/"
-#     NUMBER_OF_PAGES_TO_SEARCH = 1
-#     QUESTION_PRODUCT = "What are you looking for?\n:"
-#     PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
-#     search_term = str(bar)
+    print("")
+    print("#####################################")
+    start_time = time.time()
 
-#     biggest_discount = 0.0
-#     lowest_price = 0.0
-#     chepest_product = Product("", "", "", "", "", "")
-#     best_deal_product = Product("", "", "", "", "", "")
-#     search_terms = search_term.split(" ")
+    URL = get_url(search_term)
+    NUMBER_OF_PAGES_TO_SEARCH = 3
+    PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
 
-#     #####################################
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--headless')
-#     options.add_argument('--ignore-certificate-errors')
-#     options.add_argument('--incognito')
-#     driver = webdriver.Chrome("chromedriver.exe", options=options)
-#     #####################################
+    print("")
+    print("--- DRIVER STARTED ---")
+    print("--- %s seconds ---" % (time.time() - start_time))
+    
+    #####################################
+    options = webdriver.ChromeOptions()
+    #options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") #UNCOMMENT FOR DEPLOYMENT/COMMENT FOR TESTING
+    #####################################
+    options.add_argument("--no-sandbox")
+    options.add_argument('--headless')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--incognito")
+    #####################################
+    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options) #UNCOMMENT FOR DEPLOYMENT
+    driver = webdriver.Chrome("D:\chromedriver.exe", options=options) ##UNCOMMENT FOR TESTING (SET DRIVERT PATH)
+    #####################################
 
-#     driver.get(URL)
-#     element = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
-#     element.send_keys(search_term)
-#     element.send_keys(Keys.ENTER)
+    driver.get(URL)
 
-#     products = []
+    print("")
+    print("--- SCRAPING... ---")
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("")
 
-#     page = NUMBER_OF_PAGES_TO_SEARCH
+    products = []
 
-#     while True:
-#         if page != 0:
-#             try:
-#                 driver.get(driver.current_url + "&page=" + str(page))
-#             except:
-#                 break
+    page = NUMBER_OF_PAGES_TO_SEARCH
+
+    print("--- PAGES SCRAPED ---")
+    print(page)
+
+    while True:
+        if page != 0:
+            try:
+                driver.get(driver.current_url + "&page=" + str(page))
+            except:
+                break
         
-#         for i in driver.find_elements_by_xpath(PRODUCT_PATH):
-#             should_add = True
-#             name = ""
-#             price = ""
-#             prev_price = ""
-#             link = ""
-#             discount = 0.0
-#             # rating = ""
-#             prime = False
-#             try:
-#                 h2tag = i.find_element_by_tag_name('h2')
-#                 name = h2tag.text
-#                 price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
-#                 link = h2tag.find_element_by_tag_name('a').get_attribute("href")
-#                 # rating = 
-#                 try:
-#                     prime_element = i.find_element_by_class_name("a-icon-prime")
-#                     print(prime_element)
-#                     prime = True
-#                 except:
-#                     Exception()
-#                 try:
-#                     prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
-#                     discount = (prev_price-price)/prev_price*100
-#                 except:
-#                     Exception()
-#                     prev_price = price
-#             except:
-#                 # print("exception")
-#                 should_add = False
+        for i in driver.find_elements_by_xpath(PRODUCT_PATH):
+            should_add = True
+            name = ""
+            price = ""
+            prev_price = ""
+            link = ""
+            discount = 0.0
+            # rating = ""
+            prime = False
+            try:
+                h2tag = i.find_element_by_tag_name('h2')
+                name = h2tag.text
+                price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
+                link = h2tag.find_element_by_tag_name('a').get_attribute("href")
+                # rating = 
+                try:
+                    prime_element = i.find_element_by_class_name("a-icon-prime")
+                    prime = True
+                except:
+                    Exception()
+                try:
+                    prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
+                    discount = (prev_price-price)/prev_price*100
+                except:
+                    Exception()
+                    prev_price = price
+            except:
+                # print("exception")
+                should_add = False
             
-#             product = Product(name, price, prev_price, discount, link, prime)
-#             if should_add:
-#                 products.append(product)
-#                 # print(products)
+            product = {"Name": name, "Price": price, "Previous price": prev_price, 
+                "Discount": discount, "URL": link, "Prime product": prime}
+            if should_add:
+                products.append(product)
+                # print(products)
                 
-#         page = page - 1
-#         if page == 0:
-#             break
-#         print(page)
+        page = page - 1
+        if page == 0:
+            break
+        print(page)
+    
+    driver.quit()
 
-#     run = 0
-#     for product in products:
-#         not_right = False
-#         # for word in search_terms:
-#         #     if word.lower() not in product.name.lower():
-#         #         not_right = True
-#         if not not_right:
-#             if run == 0:
-#                 lowest_price = product.price
-#                 chepest_product = product
-#                 run = 1
-#             elif product.price < lowest_price:
-#                 lowest_price = product.price
-#                 chepest_product = product
-#             if product.discount > biggest_discount:
-#                 biggest_discount = product.discount
-#                 print(product.discount)
-#                 best_deal_product = product
+    print("")
+    print("--- SESSION COMPLETE ---")
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("")
+    print("#####################################")   
 
-#     with open('products.json', 'w') as json_file:
-#         data = {}
-#         data["Products"] = []
-#         for prod in products:
-#             data["Products"].append(prod.serialize())
-#         json.dump(data, json_file, sort_keys=True, indent=4)
+    return products
 
-#     print(json.dumps(chepest_product.serialize(), indent=4, sort_keys=True))
-#     print(json.dumps(best_deal_product.serialize(), indent=4, sort_keys=True))
+##############################################################################
 
-#     ##############################
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--ignore-certificate-errors')
-#     driver = webdriver.Chrome("chromedriver.exe", options=options)
-#     ##############################
+def best_deal(products):
+    biggest_discount = 0.0
+    lowest_price = 0.0
+    chepest_product = {}
+    best_deal_product = {}
 
-#     driver.get(best_deal_product.link)
-#     driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
+    run = 0
+    for product in products:
+        not_right = False
+        # for word in search_terms:
+        #     if word.lower() not in product.name.lower():
+        #         not_right = True
+        if not not_right:
+            if run == 0:
+                lowest_price = product["Price"]
+                chepest_product = product
+                run = 1
+            elif product["Price"] < lowest_price:
+                lowest_price = product["Price"]
+                chepest_product = product
+            if product["Discount"] > biggest_discount:
+                biggest_discount = product["Discount"]
+                best_deal_product = product
+
+    return best_deal_product
